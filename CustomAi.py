b296f1dc8ad6bb22481d977c0eb7662cf892c330
Heechan <ckstmznf@naver.com>
2023-05-13 16:35:26 +0900

    편향을 학습하는 선형회귀 모델

--- CustomAi.py
+++ CustomAi.py
@@ -1,14 +1,10 @@
 import numpy as np
 
-
 class LinearRegression:
-    import numpy as np
-
     def __init__(self):
-        self.weights = 100
-        self.bias = 0
+        self.parameters = np.array([10, 10], dtype="float64")
 
-    def get_loss(self, x, y, weight, bias):
+    def get_loss(self, x, y, weight = None):
         """
         평균 제곱 오차 방식을 사용해서 오차를 구한다.
         :param x:
@@ -18,9 +14,11 @@ class LinearRegression:
         :return:
         """
 
-        return sum((x * weight + bias - y) ** 2) / len(y) / 2
+        predict = self.predict(x, weight)
+        loss = np.sum((predict - y) ** 2) / len(y) / 2
+        return loss
 
-    def get_slope(self, x, y, weight, bias, d_x=0.0001):
+    def get_slope(self, x, y, parameter, d_x=0.0001):
         """
         측정 가중치에서의 오차 함수의 경사를 구한다.
         :param x:
@@ -30,19 +28,39 @@ class LinearRegression:
         :param d_x:
         :return:
         """
-        return (self.get_loss(x, y, weight + d_x, bias) - self.get_loss(x, y, weight, bias)) / d_x
 
-    def predict(self, x):
+        glops = np.zeros_like(parameter)
+
+        for index, weight in enumerate(parameter):
+            parameter[index] = weight + d_x
+            y1 = self.get_loss(x, y, parameter)
+
+            parameter[index] = weight - d_x
+            y2 = self.get_loss(x, y, parameter)
+
+            parameter[index] = weight
+            glops[index] = (y1 - y2) / d_x / 2
+
+        return glops
+
+    def predict(self, x, parameters = None):
         """
-        여측하는 함수
-        :param x: 예측하는 독립 변수
-        :param weight: 예측에 사용하는 가중치, 입력하지 않는다면 모델에 저장된 가중치를 사용
-        :param bias: 예측에 사용하는 편향, 입력하지 않는다면 모델에 저장된 편향을 사용
+        :param x:
+        :param parameters:
         :return:
         """
-        return x * self.weights + self.bias
 
-    def fit(self, x, y, epoch=300, learning_rate=0.001):
+        if(parameters is None):
+            weights = self.parameters[:-1]
+            bias = self.parameters[-1]
+        else:
+            weights = parameters[:-1]
+            bias = parameters[-1]
+
+
+        return x * weights + bias
+
+    def fit(self, x, y, epoch=300, learning_rate=np.array([0.001, 0.01])):
         """
         학습을 진행하는 함수
         :param x:
@@ -58,13 +76,10 @@ class LinearRegression:
             print(f"{'■' * percent_10}{'□' * (10 - percent_10)} {current}/{epoch}({percent}%%)")
 
         for i in range(epoch):
-            slop = self.get_slope(x, y, self.weights, self.bias)
-            self.weights -= slop * learning_rate
+            slop = self.get_slope(x, y, self.parameters)
+            self.parameters -= slop * learning_rate
 
             print_loading_bar(i + 1, epoch)
-
-
-
 
     def evaluate(self):
         # Todo("평가 함수 작성")
--- main.py
+++ main.py
@@ -2,23 +2,31 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+import cv2
+
 from CustomAi import LinearRegression
 
 if __name__ == '__main__':
     data = pd.read_csv("./data/Fish.csv")
     data = data[data["Species"] == "Perch"]
 
-    input_data = data["Length"].to_numpy()
-    target_data = data["Weight"].to_numpy()
+    input_data = data["Length"].to_numpy().reshape(-1, 1)
+    target_data = data["Weight"].to_numpy().reshape(-1, 1)
 
     model = LinearRegression()
+    model.fit(input_data, target_data, epoch=3000)
+    loss = model.get_loss(input_data, target_data)
 
-    model.fit(input_data, target_data, 3000, 0.00001)
+    plt.xlim(0, 50)
+    plt.ylim(0, 1500)
 
-    x = np.arange(min(input_data), max(input_data))
-    y = x * model.weights + model.bias
+    weight = model.parameters[0]
+    bias = model.parameters[1]
 
-    plt.plot(x, y, "r--")
+    x = np.arange(0, 50)
+    y = x * weight + bias
+
     plt.scatter(input_data, target_data)
-
+    plt.plot(x, y, "r--")
+    plt.text(2, 1400, f"y = {round(weight, 2)} * X + {round(bias, 2)}, (loss : {round(loss, 2)})", color="red")
     plt.show()
